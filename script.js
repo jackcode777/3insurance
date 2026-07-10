20c38cb36b34cb090457a30609ed4decf32d24f0
jackcode777 <jack1662009777@gmail.com>
2026-07-10 09:36:16 +0800

    Refactor button click handler to include courier input

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => {
      let input = Number(document.getElementById("input").value);
+     let courierInput = Number(document.getElementById("courier").value);
      let errorMsg = document.querySelector("#error");
      let result = document.getElementById("op");
+     let inResult = document.getElementById("insurance");
      let raw = Number(input.innerHTML = "");
      //error handeling
      if (input === raw || isNaN(input)) {
@@ -13,9 +15,11 @@ btn.addEventListener("click", () => {
      }
      function answer(x) {
           let xx = x;
+          let final = xx + courierInput;
           errorMsg.innerHTML = "";
           result.style.color = "green";
-          result.innerHTML = "RM " + xx.toFixed(2);
+          result.innerHTML = "RM " + final.toFixed(2);
+          inResult.innerHTML = "RM " + xx.toFixed(2);
      }
-     let opp = answer((input / 100) * 3);
+     let ansField = answer((input / 100) * 3);
 }); 
